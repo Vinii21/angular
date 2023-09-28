4eb24abc9706f04ddaef86f633b18a747d286bce
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-09-28 13:21:50 -0600

    Directiva ngIf

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -9,5 +9,25 @@ export class HeroComponent {
   public name:string = "iron-man";
   public age: number = 45;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${this.name} ${this.age}`;
+  }
+
+  changeHero():void {
+    this.name = "Spiderman"
+  }
+
+  changeAge():void {
+    this.age = 25
+  }
+
+  reset():void {
+    this.name = "iron-man";
+    this.age = 45;
+  }
 
 }
