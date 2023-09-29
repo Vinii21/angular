c65159708a77268387cf2c312e69779b725072ec
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-09-29 09:06:22 -0600

    ngFor, ngIf;else , ng-template

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  public heroNames: string[] = ["Batman", "Superman", "Linterna Verde", "Mujer Maravilla"];
+  public deletedHero?: string;
+
+  removeLastHero():void {
+    this.deletedHero = this.heroNames.pop();
+  }
+
 
 }
