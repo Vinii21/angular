5fde0420d0685de7351d6236f3f2c1c9a03eca22
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-02 08:55:44 -0600

    Emitiendo el index de la lista para borrar un elemento con @output()

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -9,6 +9,13 @@ import { Character } from '../../interfaces/character.interface';
 export class ListComponent {
 
   @Input()
-  public characterList: Character[] = []
+  public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteId : EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number):void {
+    this.onDeleteId.emit(index);
+  }
 
 }
